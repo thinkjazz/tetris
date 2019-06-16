dc5055e0965b7d97ff8859603d90ec9648bac658
lookrecius <bragin.boris@ya.ru>
2019-06-17 02:35:11 +0300

    Added rotationPeace function

--- src/game.js
+++ src/game.js
@@ -29,13 +29,17 @@ export default class Game {
     activePiece = {
         x: 0,
         y: 0,
+        get blocks() {
+            return this.rotations[this.rotationsIndex];
+        },
         blocks: [
             [0, 1, 0],
             [1, 1, 1],
             [0, 0, 0]
         ]
+    
 
-    }
+    };
 
     movePieceLeft() {
         this.activePiece.x -= 1;
@@ -59,6 +63,74 @@ export default class Game {
             this.lockPiece();
         }
 
+    }
+
+    rotatePeace() {
+        this.rotateBlock();
+    //   const blocks = this.activePiece.blocks;
+    //   const length  = blocks.length;
+
+    //   const x  = Math.floor(length / 2);
+    //   const y  = length - 1;
+    //   for (let i = 0; i < x; i++) {
+    //     for (let j = i; j < y - i; j++) {
+    //         const temp = blocks[i][j];
+
+    //         blocks[i][j] = blocks[y - j][i];
+    //         blocks[y - j][i] = blocks[y - i][y - j];
+    //         blocks[y - i][y - j] = blocks[j][y - i];
+    //         blocks[j][y - i] = temp;
+            
+    //     }   
+    // }
+    // // Вторая система ротации
+    //   const temp = [];
+    //   for (let i = 0; i < length; i++) {
+    //     temp[i] = new Array(length).fill(0);
+          
+    //   }
+
+    //   for (let y = 0; y < length; y++) {
+    //         for (let x = 0; x < length; x++) {
+    //             temp[x][y] = blocks[length - 1 - y][x];
+                
+    //         }
+          
+    //   }
+
+    //    this.activePiece.blocks = temp;  
+
+    //    if (this.hasCollision()) {
+    //        this.activePiece.blocks = blocks;
+    //    }
+        if (this.hasCollision()) {
+            this.rotateBlock(false);
+        }       
+    }
+
+    rotateBlock(clockwise = true){
+        const blocks = this.activePiece.blocks;
+        const length  = blocks.length;
+  
+        const x  = Math.floor(length / 2);
+        const y  = length - 1;
+        for (let i = 0; i < x; i++) {
+          for (let j = i; j < y - i; j++) {
+              const temp = blocks[i][j];
+  
+              if (clockwise) {
+                blocks[i][j] = blocks[y - j][i];
+                blocks[y - j][i] = blocks[y - i][y - j];
+                blocks[y - i][y - j] = blocks[j][y - i];
+                blocks[j][y - i] = temp;
+               } else {
+                blocks[i][j] = blocks[j][y - i];
+                blocks[j][y - i] = blocks[y - i][y - j];
+                blocks[y - i][y - j] = blocks[y - j][i];
+                blocks[y - j][i] = temp;
+               }
+            }   
+        }
     }
 
     hasCollision() {
